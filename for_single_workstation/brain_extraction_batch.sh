aaf8e3bef9e0c75897eff7fed98090848ddc8bce
dancebean <madamotor@hotmail.com>
2014-02-18 16:51:26 +0000

    Version 1.0.1

--- for_single_workstation/brain_extraction_batch.sh
+++ for_single_workstation/brain_extraction_batch.sh
@@ -1,8 +1,9 @@
-export QSUB_CMD="qsub -l h_rt=1:00:00 -pe smp 1 -R y -l h_vmem=1G -l tmem=1G -j y -S /bin/sh -b y -cwd -V -o job_output -e job_error" #  -l s_stack=128M
-
 # $1: folder containing the original images
 # $2: folder containing the brain masks
 # $3: folder to put the extracted brain images
+
+# export QSUB_CMD="qsub -l h_rt=1:00:00 -pe smp 1 -R y -l h_vmem=1G -l tmem=1G -j y -S /bin/sh -b y -cwd -V -o job_output -e job_error" #  -l s_stack=128M
+
 if [ ! -d job_output ]
 then mkdir job_output
 fi
@@ -15,6 +16,6 @@ fi
 
 for G in `ls $1`
 do
-  IMG=`echo "$G" | cut -d'.' -f1`
-  ${QSUB_CMD} -N extract_$G seg_maths -in $1/${IMG} -mul $2/${IMG} -out $3/${IMG}.nii.gz
+  MASK=`echo "$G" | cut -d'.' -f1`
+  seg_maths $1/${MASK} -mul $2/${MASK} $3/${MASK}.nii.gz
 done
