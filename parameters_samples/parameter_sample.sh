71c506817ff20a63b6881878c1ba54a630bd3a4c
Da Ma <dma73@rcg-visnode-02.rcg.sfu.ca>
2019-05-24 00:22:05 -0700

    update parameter file

--- parameters_samples/parameter_sample.sh
+++ parameters_samples/parameter_sample.sh
@@ -1,5 +1,7 @@
-#
-affine_param=" -rigOnly "
+# affine transformation parameter for masking
+# -nac: initialize transformation with nifty header (default: centre of image)
+# -cog: centor of mass/gravity of mask to initialize transformation (default: centre of image)
+affine_param=" -rigOnly" # -nac
 #
 resamp_param=""
 #
